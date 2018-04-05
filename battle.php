85f10233110d9553e498c81e43b4cf68db5741d4
Jennifer Lieby <ze_nifer@hotmail.com>
2018-04-05 20:44:06 +0200

    animations à la con page accueil et perso

---
+++ battle.php
@@ -0,0 +1,50 @@
+<?php
+
+ ?>
+
+ <!DOCTYPE html>
+ <html lang="en" dir="ltr">
+   <head>
+     <meta charset="utf-8">
+     <title>Hackathon Battle</title>
+     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+     <meta name="description" content="">
+     <meta name="author" content="">
+
+     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+
+     <!-- Custom styles for this template -->
+     <link href="css/style.css" rel="stylesheet">
+   </head>
+
+   <body>
+
+     <div class="row accueil">
+
+       <div class="card" style="width: 25%;">
+         <img class="card-img-top" src="" alt="Card image cap">
+         <div class="card-body">
+           <h5 class="card-title"><?php echo 'Test' ?></h5>
+           <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+           <div class="bouton">
+             <a href="#" class="btn btn-primary">Choose</a>
+           </div>
+         </div>
+       </div>
+
+       <div class="card" style="width: 25%;">
+         <img class="card-img-top" src="" alt="Card image cap">
+         <div class="card-body">
+           <h5 class="card-title"><?php echo 'Test' ?></h5>
+           <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+           <div class="bouton">
+             <a href="#" class="btn btn-primary">Choose</a>
+           </div>
+         </div>
+       </div>
+
+     </div>
+
+   </body>
+
+</html>
--- personnages.php
+++ personnages.php
@@ -39,48 +39,76 @@ echo $response[2];*/
 
     <!-- Custom styles for this template -->
     <link href="css/style.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
+      <link rel="stylesheet" href="css/font-css.css">
   </head>
   <body>
 
-    <div class="container">
-      <div class="row">
+    <div class="total">
 
-        <div class="card" style="width: 25%;">
-          <img class="card-img-top" src="" alt="Card image cap">
-          <div class="card-body">
-            <h5 class="card-title"><?php echo 'Test' ?></h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Choose</a>
+      <div class="accueil">
+        <img src="image/street-fighter.png" class="image-accueil" alt="">
+      </div>
+
+      <br>
+      <div class="accueil">
+        <h1>
+          <span class="glitch" data-text="ChOose your fighteR">
+            Choose your fighter !
+          </span>
+        </h1>
+      </div>
+
+      <br><br>
+
+      <div class="container">
+        <div class="row accueil">
+
+          <div class="card" style="width: 25%;">
+            <img class="card-img-top" src="" alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title"><?php echo 'Test' ?></h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <div class="bouton">
+                <a href="#" class="btn btn-primary">Choose</a>
+              </div>
+            </div>
           </div>
-        </div>
 
-        <div class="card" style="width: 25%;">
-          <img class="card-img-top" src="" alt="Card image cap">
-          <div class="card-body">
-            <h5 class="card-title"><?php echo 'Test' ?></h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Choose</a>
+          <div class="card" style="width: 25%;">
+            <img class="card-img-top" src="" alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title"><?php echo 'Test' ?></h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <div class="bouton">
+                <a href="#" class="btn btn-primary">Choose</a>
+              </div>
+            </div>
           </div>
-        </div>
 
-        <div class="card" style="width: 25%;">
-          <img class="card-img-top" src="" alt="Card image cap">
-          <div class="card-body">
-            <h5 class="card-title"><?php echo 'Test' ?></h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Choose</a>
+          <div class="card" style="width: 25%;">
+            <img class="card-img-top" src="" alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title"><?php echo 'Test' ?></h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <div class="bouton">
+                <a href="#" class="btn btn-primary">Choose</a>
+              </div>
+            </div>
           </div>
-        </div>
 
-        <div class="card" style="width: 25%;">
-          <img class="card-img-top" src="" alt="Card image cap">
-          <div class="card-body">
-            <h5 class="card-title"><?php echo 'Test' ?></h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Choose</a>
+          <div class="card" style="width: 25%;">
+            <img class="card-img-top" src="" alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title"><?php echo 'Test' ?></h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <div class="bouton">
+                <a href="#" class="btn btn-primary">Choose</a>
+              </div>
+            </div>
           </div>
-        </div>
 
+        </div>
       </div>
     </div>
 
