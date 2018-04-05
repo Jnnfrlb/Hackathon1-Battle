6d16eb4bbdce84b0bba56bd6eb38a6cdb7884794
Cyril Bellamy <cyrilbellamy1@gmail.com>
2018-04-05 20:21:14 +0200

    Mise en place de récupération de données dans page stats.php

---
+++ stats.php
@@ -0,0 +1,13 @@
+<?php
+
+$json = file_get_contents("https://akabab.github.io/superhero-api/api/id/150.json");
+$tab=json_decode($json,true);
+echo "<br><br>";
+
+echo "<h3>".$tab['name']."</h3>";
+foreach ($tab['powerstats'] as $key => $value) {
+    echo $key .":"."<br>"."<progress value=".$value." max=\"100\"></progress><br>";
+};
+
+?>
+
